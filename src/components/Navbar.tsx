5206e0b45fc6f119f12fecc8280bff7c2da54645
MenotiFilho <menotimfilho@gmail.com>
2022-02-07 23:56:47 -0300

    teste

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export function Navbar(){
+    return(
+        <div
+        style={{
+            display:"flex"
+        }}
+        >
+            <div 
+            style={{
+                background: 'blue',
+                Height: '10px'
+            }}
+            ></div>
+            <div></div>
+        </div>
+    )
+}
